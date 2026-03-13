df77a530b13934ecbf0a87d1bb2d6790afa12496
Tharsi <tharsigaranganathan17@gmail.com>
2026-03-13 19:54:39 +0530

    build: integrate Mochawesome reporting and test report scripts

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mocha',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
   },
 });
